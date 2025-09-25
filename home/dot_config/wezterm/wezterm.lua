93bfec916643410382e0861ddc3f309662c329b5
Per Pettersson <Per.Pettersson@gmail.com>
2025-09-24 21:50:35 -0400

    updated wezterm config

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -11,6 +11,32 @@ end
 -- Pull in WezTerm API
 local wezterm = require("wezterm")
 
+-- Title
+function basename(s)
+	return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local pane = tab.active_pane
+
+	local index = ""
+	if #tabs > 1 then
+		index = string.format("%d: ", tab.tab_index + 1)
+	end
+
+	local process = basename(pane.foreground_process_name)
+
+	return { {
+		Text = " " .. index .. process .. " ",
+	} }
+end)
+
+-- Startup
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
 -- Initialize actual config
 local config = {}
 if wezterm.config_builder then
@@ -23,16 +49,27 @@ config.font = wezterm.font_with_fallback({
 	{ family = "MonoLisa", weight = "DemiBold" },
 })
 
+config.native_macos_fullscreen_mode = true
+
 -- config.font = wezterm.font 'JetBrainsMono Nerd Font'
 -- config.font = wezterm.font("MonoLisa")
 config.font_size = 20.0
 config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
-config.window_decorations = "RESIZE"
+config.line_height = 1.2
+-- config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
-config.native_macos_fullscreen_mode = false
+config.use_fancy_tab_bar = false
+
+config.native_macos_fullscreen_mode = true
 
 config.window_background_opacity = 0.9
 config.text_background_opacity = 0.7
+
+-- Tab bar
+config.enable_tab_bar = true
+config.show_tab_index_in_tab_bar = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 25
 
 -- Keybindings
 config.keys = {
@@ -44,14 +81,24 @@ config.keys = {
 		action = wezterm.action.QuickSelect,
 	},
 	{
-		key = "|",
-		mods = "LEADER|SHIFT",
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		key = "d",
+		mods = "CMD",
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
 	{
-		key = "-",
-		mods = "LEADER",
+		key = "d",
+		mods = "CMD|SHIFT",
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "t",
+		mods = "CTRL",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
 	},
 }
 
